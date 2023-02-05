03b321a48e724a8959769c9c09dcfe224308e693
huyvu8051 <huyvu8051@gmail.com>
2023-02-05 18:47:28 +0700

    response body streaming api

--- backend/src/main/java/io/huyvu/reboot/backend/controller/HomeController.java
+++ backend/src/main/java/io/huyvu/reboot/backend/controller/HomeController.java
@@ -3,24 +3,63 @@ package io.huyvu.reboot.backend.controller;
 import com.github.javafaker.Faker;
 import io.huyvu.reboot.backend.dto.Student;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+@CrossOrigin
 @RestController
+@RequestMapping("/api")
 public class HomeController {
 
     Faker faker = new Faker(new Locale("vi-VN"));
 
-    @GetMapping(value = "/home", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
-    Flux<Student> home(){
-         return Flux.interval(Duration.ofMillis(101)).map(i -> new Student(faker.idNumber().valid(), faker.name().fullName(), faker.university().name().toString(), faker.number().numberBetween(16, 99)));
+    @GetMapping(value="/data")
+    public ResponseEntity<StreamingResponseBody> streamData() {
+        StreamingResponseBody responseBody = response -> {
+            for (int i = 1; i <= 1000; i++) {
+                try {
+                    Thread.sleep(10);
+                    response.write(("Data stream line - " + i + "\n").getBytes());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(responseBody);
     }
+
+    @GetMapping(value = "/flux", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    Flux<Student> home(){
+         return Flux.interval(Duration.ofMillis(200)).map(i -> new Student(faker.idNumber().valid(), faker.name().fullName(), faker.university().name().toString(), faker.number().numberBetween(16, 99)));
+    }
+
+    @GetMapping(value = "/srb")
+    ResponseEntity<StreamingResponseBody> srb(){
+        StreamingResponseBody stream = out -> {
+            for (int i = 0; i < 1000; i++){
+                String msg = "/srb" + " @ " + new Date();
+                out.write(msg.getBytes());
+
+            }
+        };
+        return ResponseEntity.ok(stream);
+    }
+
+
+
     @GetMapping(value = "/ls", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     List<Student> list(){
         List<Student> rs = new ArrayList<>();
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,159 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import {useEffect} from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const columns = [
+    {id: 'id', label: 'Identify', minWidth: 170},
+    {id: 'name', label: 'Full name', minWidth: 100},
+    {
+        id: 'clazz',
+        label: 'Class',
+        minWidth: 170,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+        id: 'age',
+        label: 'Age',
+        minWidth: 170,
+        align: 'right',
+        format: (value) => value.toFixed(2),
+    },
+];
+
+function createData(name, code, population, size) {
+    const density = population / size;
+    return {name, code, population, size, density};
 }
 
-export default App;
+const rows = [
+    createData('India', 'IN', 1324171354, 3287263),
+    createData('China', 'CN', 1403500365, 9596961),
+    createData('Italy', 'IT', 60483973, 301340),
+    createData('United States', 'US', 327167434, 9833520),
+    createData('Canada', 'CA', 37602103, 9984670),
+    createData('Australia', 'AU', 25475400, 7692024),
+    createData('Germany', 'DE', 83019200, 357578),
+    createData('Ireland', 'IE', 4857000, 70273),
+    createData('Mexico', 'MX', 126577691, 1972550),
+    createData('Japan', 'JP', 126317000, 377973),
+    createData('France', 'FR', 67022000, 640679),
+    createData('United Kingdom', 'GB', 67545757, 242495),
+    createData('Russia', 'RU', 146793744, 17098246),
+    createData('Nigeria', 'NG', 200962417, 923768),
+    createData('Brazil', 'BR', 210147125, 8515767),
+];
+
+export default function StickyHeadTable() {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [dataList, setDataList] = React.useState([]);
+
+
+    useEffect(() => {
+
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://localhost:8080/api/flux');
+        xhr.seenBytes = 0;
+
+        xhr.onreadystatechange = function () {
+            // console.log("state change.. state: "+ xhr.readyState);
+
+            if (xhr.readyState === 3) {
+                var newData = xhr.response.substr(xhr.seenBytes);
+                // console.log("newData: <<" +newData+ ">>");
+                //document.body.innerHTML += "New data: <<" +newData+ ">><br />";
+
+                try {
+                    const parse = JSON.parse(newData);
+                    dataList.push(parse)
+                    setDataList(current => [...current, parse])
+
+                } catch (e) {
+
+                }
+
+                xhr.seenBytes = xhr.responseText.length;
+                //console.log("seenBytes: " +xhr.seenBytes);
+            }
+        };
+
+        xhr.addEventListener("error", function (e) {
+            console.log("error: " + e);
+        });
+
+        console.log(xhr);
+        xhr.send();
+
+    }, [])
+
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+    return (
+        <Paper sx={{width: '100%', overflow: 'hidden'}}>
+            <TableContainer sx={{maxHeight: 440}}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{minWidth: column.minWidth}}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            dataList
+
+                                .map((row) => {
+                                    return (
+                                        <TableRow hover role="checkbox" tabIndex={-1} key={row.id  }>
+                                            {columns.map((column) => {
+                                                const value = row[column.id];
+                                                return (
+                                                    <TableCell key={column.id} align={column.align}>
+                                                        {column.format && typeof value === 'number'
+                                                            ? column.format(value)
+                                                            : value}
+                                                    </TableCell>
+                                                );
+                                            })}
+                                        </TableRow>
+                                    );
+                                })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+        </Paper>
+    );
+}

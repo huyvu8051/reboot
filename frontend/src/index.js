4757eaafa043690e0f8758ccee300e3c8692bbb7
huyvu8051 <huyvu8051@gmail.com>
2023-02-10 23:50:10 +0700

    fix index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,42 +4,36 @@ import reportWebVitals from './reportWebVitals';
 
 import {persistor, store} from "./store";
 import {CircularProgress} from "@mui/material";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import SignIn from "./view/SignIn";
-import PrivateRoute from "./component/PrivateRoute";
-import {Home} from "./view/Home";
+import {BrowserRouter} from "react-router-dom";
 import {SnackbarProvider} from "notistack";
 import {SnackbarUtilsConfiguration} from "./util/snackbarUtils";
 import {Provider} from "react-redux";
 import {PersistGate} from "redux-persist/integration/react";
+import {GoogleOAuthProvider} from '@react-oauth/google';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
+import MyRoute from "./route/MyRoute";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <PersistGate loading={<CircularProgress/>} persistor={persistor}>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path={'/signin'}
-                               element={<SignIn/>}
+        <GoogleOAuthProvider clientId="653266560923-h4op46mflhjpa28879oanln3382qlkt5.apps.googleusercontent.com">
+            <Provider store={store}>
+                <PersistGate loading={<CircularProgress/>} persistor={persistor}>
 
-                        />
-                        <Route path="/home" element={<PrivateRoute><Home/></PrivateRoute>}></Route>
+                    <BrowserRouter>
+                        <MyRoute/>
 
-                    </Routes>
-
-                </BrowserRouter>
-                <SnackbarProvider maxSnack={7} anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
-                    <SnackbarUtilsConfiguration/>
-                </SnackbarProvider>
-            </PersistGate>
-        </Provider>
+                    </BrowserRouter>
+                    <SnackbarProvider maxSnack={7} anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
+                        <SnackbarUtilsConfiguration/>
+                    </SnackbarProvider>
+                </PersistGate>
+            </Provider>
+        </GoogleOAuthProvider>
     </React.StrictMode>
 );
 

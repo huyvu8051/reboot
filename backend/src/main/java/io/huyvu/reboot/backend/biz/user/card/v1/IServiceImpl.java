e1caf6ff2d26e83ababe972328c7fe5d6cbbd723
huyvv <huyvu8051@gmail.com>
2023-05-12 11:52:25 +0700

    configable upload dir in application.properties

--- backend/src/main/java/io/huyvu/reboot/backend/biz/user/card/v1/IServiceImpl.java
+++ backend/src/main/java/io/huyvu/reboot/backend/biz/user/card/v1/IServiceImpl.java
@@ -5,6 +5,7 @@ import io.huyvu.reboot.backend.util.FileUploadUtil;
 import io.socket.socketio.server.SocketIoNamespace;
 import io.socket.socketio.server.SocketIoServer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,11 +20,11 @@ import static io.huyvu.reboot.backend.util.JsonUtils.toJsonObjExcludeNull;
 @RequiredArgsConstructor
 @Service
 public class IServiceImpl implements IService {
-    private final String UPLOAD_DIR = "D://reboot/resources";
-    private static final String DASHBOARD = "/dashboard";
+
+    @Value("${reboot.upload.dir}")
+    private String UPLOAD_DIR;
 
     private final IRepository repository;
-    private final SocketIoServer sioServer;
 
     private final DashboardBroadcast dashboardBroadcast;
 
@@ -35,25 +36,23 @@ public class IServiceImpl implements IService {
     @Override
     public void updateCardDetails(UpdateCardDetailsReq req) {
         repository.updateCardDetails(req);
-        long wpId = repository.selectWpId(req.id());
-        SocketIoNamespace nsp = sioServer.namespace(DASHBOARD);
-        nsp.broadcast(String.valueOf(wpId), "update.dashboard.card", toJsonObjExcludeNull(req));
+        dashboardBroadcast.byCardId(req.id(), "update.dashboard.card", toJsonObjExcludeNull(req));
     }
 
     @Override
     public String uploadAttachment(MultipartFile file, long cId) {
         String originalFileName = StringUtils.cleanPath(file.getOriginalFilename());
+        var extensionIndex = originalFileName.lastIndexOf(".");
+        String storeFileName = UUID.randomUUID() + originalFileName.substring(extensionIndex);
+
         long bId = repository.selectBoardId(cId);
-        String storeFileName = UUID.randomUUID() + originalFileName.substring(originalFileName.lastIndexOf("."));
         FileUploadUtil.saveFile(UPLOAD_DIR + "/" + bId, storeFileName, file);
 
-        var fileExtension = originalFileName.substring(originalFileName.lastIndexOf(".") + 1);
+        var fileExtension = originalFileName.substring(extensionIndex + 1);
         var attachmentId = repository.insertAttachment(cId, originalFileName, storeFileName, fileExtension);
 
         AttachmentVo attachment =  repository.selectAttachment(attachmentId);
-
         dashboardBroadcast.byCardId(cId, "update.dashboard.attachment", toJsonObj(attachment));
-
         return storeFileName;
     }
 }
--- backend/src/main/java/io/huyvu/reboot/backend/config/resource/ResourceHandleRegistry.java
+++ backend/src/main/java/io/huyvu/reboot/backend/config/resource/ResourceHandleRegistry.java
@@ -5,6 +5,7 @@
 package io.huyvu.reboot.backend.config.resource;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,11 +13,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class ResourceHandleRegistry implements WebMvcConfigurer {
-    private final String UPLOAD_DIR = "file:D:/reboot/resources/";
+    @Value("${reboot.upload.dir}")
+    private String UPLOAD_DIR;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/api/v1/internal/resources/board/**")
-                .addResourceLocations(UPLOAD_DIR);
+                .addResourceLocations("file:" + UPLOAD_DIR + "/");
     }
 }

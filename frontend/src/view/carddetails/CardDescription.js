234d9026a14ca7906e00883d9e51cb54e34c2c8d
huyvv <huyvu8051@gmail.com>
2023-05-15 11:11:56 +0700

    add description ui

--- frontend/src/view/carddetails/CardDescription.js
+++ frontend/src/view/carddetails/CardDescription.js
@@ -5,9 +5,12 @@ import EditorJS from '@editorjs/editorjs'
 import './CardDescription.css'
 
 import config from './editor-js-tools-config'
+import api from "../../service/api";
+import {useSelector} from "react-redux";
 
 const CardDescription = () => {
     const editorContainerRef = useRef()
+    const item = useSelector(sts => sts.dashboard.card)
 
     const [focused, setFocused] = useState(false)
 
@@ -17,28 +20,47 @@ const CardDescription = () => {
             holder: editorContainerRef.current,
             minHeight: '0px',
             placeholder: 'Type here...',
-            tools: config
+            tools: config,
+            data: JSON.parse(item.description)
         })
         const editorElement = editorContainerRef.current
-        const onFocusIn = () => setFocused(true)
-        const onFocusOut = () => setFocused(false)
+        const onFocusIn = ({currentTarget, relatedTarget}) => {
+            if (currentTarget.contains(relatedTarget)) return
+            setFocused(true);
+
+        }
+        const onFocusOut = ({currentTarget, relatedTarget}) => {
+            if (currentTarget.contains(relatedTarget)) return
+            setFocused(false);
+
+            editor.save().then(out => {
+                const data = JSON.stringify(out);
+                console.log(JSON.parse(data))
+                /*api.put('/api/v1/user/dashboard/card/details', {
+                    id: item.id,
+                    description: data
+                }).then()*/
+            })
+
+        }
         editorElement.addEventListener("focusin", onFocusIn)
         editorElement.addEventListener("focusout", onFocusOut)
         return () => {
             editorElement.removeEventListener("focusin", onFocusIn)
             editorElement.removeEventListener("focusout", onFocusOut)
+            editor.destroy()
         }
 
-    }, [])
 
-    console.log('render')
+    }, [item])
+
     return <>
         <h5 style={{padding: 0, margin: 0}}>
             Description
         </h5>
         <div style={{
             marginLeft: -5,
-            padding: 5,
+            padding: focused ? 3 : 5,
             borderRadius: 5,
             border: focused ? '2px solid #1976d2' : 0,
         }} ref={editorContainerRef}/>
--- frontend/src/view/carddetails/CardHeaderCover.js
+++ frontend/src/view/carddetails/CardHeaderCover.js
@@ -12,7 +12,6 @@ const CardHeaderCover = ({handleClose}) => {
 
     const bId = useSelector(sts => sts.dashboard.board?.id)
     const card = useSelector(sts => sts.dashboard.card || {})
-    console.log(card)
 
     const handleFileChange = e => {
 

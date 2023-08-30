02d1253273ed8b6a86d0cd5f13c0f51c10d9d7f6
huyvu8051 <huyvu8051@gmail.com>
2023-08-30 23:41:58 +0700

    change somethink

---
+++ reboot-message-biz/src/main/java/io/huyvu/reboot/message/model/InsertGrpMemDTO.java
@@ -0,0 +1,2 @@
+package io.huyvu.reboot.message.model;public class InsertGrpMemDTO {
+}
---
+++ reboot-message-biz/src/main/java/io/huyvu/reboot/message/socket/MessageSocketCfg.java
@@ -0,0 +1,31 @@
+/**
+ * @Author HuyVu
+ * @CreatedDate 3/1/2023 9:41 AM
+ */
+package io.huyvu.reboot.message.socket;
+
+import io.socket.socketio.server.SocketIoNamespace;
+import io.socket.socketio.server.SocketIoServer;
+import io.socket.socketio.server.SocketIoSocket;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class SocketCfg {
+
+    @Bean
+    ApplicationRunner config(SocketIoServer sioServer) {
+        return args -> {
+            SocketIoNamespace nsp = sioServer.namespace("/message");
+
+            nsp.on("connection", args2 -> {
+                SocketIoSocket socket = (SocketIoSocket) args2[0];
+                String id = socket.getId();
+                log.info("client {} connected", id);
+            });
+        };
+    }
+}

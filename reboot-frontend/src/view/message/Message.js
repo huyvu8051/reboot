fe1ac287abb4ac048cca150288bfd0f8cf9a23cf
Vũ Văn Huy <huyvu8051@gmail.com>
2023-12-15 10:08:21 +0700

    change somethink

--- reboot-frontend/src/view/message/Message.js
+++ reboot-frontend/src/view/message/Message.js
@@ -1,31 +1,16 @@
 import {Box, IconButton} from '@mui/material'
 import {useEffect} from 'react'
 import {useDispatch} from 'react-redux'
-import {io} from 'socket.io-client'
-import Conversations from './Conversations'
-import {saveMsg} from './message-slice'
+import {useParams} from 'react-router-dom'
+import api from '../../service/api'
+import {save} from './message-slice'
 
 function Message(props) {
-
     const dispatch = useDispatch()
+    const {bId, wId, cId, convId} = useParams()
     useEffect(() => {
-        const socket = io('/chat', {
-         /*   query: {
-                wId: wId
-            },
-            transports: ['websocket']*/
-        })
-        socket.on('sendMsg', r => {
-            dispatch(saveMsg(r))
-        })
-
-
-        return () => {
-            socket.off()
-            socket.disconnect()
-        }
-
-    }, [dispatch])
+        api.post('/api/v1/message/init', {bId, wId, cId, convId}).then(r => dispatch(save(r)))
+    }, [bId, wId, cId, convId, dispatch])
 
     return <>
         <Box height="100%">
@@ -37,7 +22,6 @@ function Message(props) {
             }}
             >
             </IconButton>
-            <Conversations/>
             <Box sx={{
                 ml: '226.078px',
                 height: '100%',
